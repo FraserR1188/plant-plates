56a7258249caf3c45e3ad58257f5810f48f65a38
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2025-03-01 15:24:23 +0000

    add test.html

--- plantplates/routes.py
+++ plantplates/routes.py
@@ -5,4 +5,4 @@ from plantplates.models import User, Category, Recipe, Review, Article
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("test.html")
