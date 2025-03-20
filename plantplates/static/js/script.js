d1675030ecafb92b24c29441a94c2eac8959d75d
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2025-03-20 20:52:27 +0000

    Modal for deletion added to confirm user wants to delete

--- plantplates/static/js/script.js
+++ plantplates/static/js/script.js
@@ -46,3 +46,34 @@ document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll("select");
   M.FormSelect.init(elems);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize modal(s)
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems);
+
+  // Attach event listener to delete buttons
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+  deleteButtons.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      // Retrieve category id and name from data attributes
+      var categoryId = btn.getAttribute("data-category-id");
+      var categoryName = btn.getAttribute("data-category-name");
+
+      // Update the modal message
+      document.getElementById("deleteMessage").textContent =
+        "Are you sure you want to delete the category '" +
+        categoryName +
+        "'? This action will delete all recipes in this category.";
+
+      // Dynamically update the form action URL
+      // The placeholder '__placeholder__' will be replaced with the actual categoryId.
+      var deleteUrl =
+        "{{ url_for('delete_category', category_id='__placeholder__') }}".replace(
+          "__placeholder__",
+          categoryId
+        );
+      document.getElementById("deleteForm").action = deleteUrl;
+    });
+  });
+});
